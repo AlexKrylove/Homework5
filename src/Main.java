dc8e277a0ef9b59edbf9733b3123a21986f24e67
AlexKrylov <krylofffff93@gmail.com>
2022-10-26 20:36:59 +0300

    Task1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task1");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию для IOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию для Андроид по ссылке");
+        }
+    }
+}
