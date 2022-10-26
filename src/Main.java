1aab4ff040b1e62dc81a07a8af562bb325d18e06
AlexKrylov <krylofffff93@gmail.com>
2022-10-26 22:04:02 +0300

    Task3

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,11 @@ public class Main {
         }
         //
         System.out.println("Task3");
-
+        int year = 2021;
+        if (year % 4 == 0 && year % 400 != 0) {
+            System.out.println(year + " год високосный");
+        } else {
+            System.out.println(year + " год не високосный");
+        }
     }
 }
